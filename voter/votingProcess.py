1db028e885e71eb2726f50304c7059fce031baba
Cayla-Ginestet Thomas <caylagin@insa-10583>
2023-04-14 14:35:21 +0200

    lien interface/BDD pour la partie vote OK

--- voter/votingProcess.py
+++ voter/votingProcess.py
@@ -1,4 +1,5 @@
 import socket
+import gestiondb as db
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
@@ -9,12 +10,7 @@ def vote():
     while not validvote:
         print("Here are the candidates : ")
         # récup la liste des candidats à partir de la bdd
-        candidates = {
-            1 : "Alata",
-            2 : "Migliore",
-            3 : "Bit-Monnot",
-            4 : "Nicomette"
-        }
+        candidates = db.getCandidates()
         print(candidates)
         vote=int(input("Enter your vote (number from 1 to 4) : "))
         
@@ -36,13 +32,13 @@ def vote():
                     print("Invalid answer. Please enter y for yes and n for no.")
     print ("Your vote has been casted. Goodbye.")
     
+    #partie TCP
     # chiffrer vote => Pailler
-    
+    """
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((TCP_IP, TCP_PORT))
     s.send(bytes(str(vote),"utf-8"))
     data = s.recv(BUFFER_SIZE)
     print( "received data:\n", data.decode("utf-8"))
-    s.close()
-                
-     
+    s.close()  
+    """
