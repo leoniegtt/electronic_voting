3c0081746d2b39feae5bff3f9f730ea7d7d73cca
leoniegtt <leonie.goutte@gmail.com>
2023-04-25 21:13:31 +0200

    ++

--- Serveur_Urne/Pailler.py
+++ Serveur_Urne/Pailler.py
@@ -1,4 +1,5 @@
-# pip install paillierlib
+#!/usr/bin/env python
+
 from paillierlib import paillier
 from gmpy2 import mpz
 
@@ -13,8 +14,9 @@ def initialize():
     nb_of_candidates = 3
 
     #choose the number assigned to candidates depending on how many people vote
-    for i in range(nb_of_candidates.__sizeof__):
-        candidate_i=mpz(pow(10,i+2)) # the number assigned to the candidate is 10^i+2
+    candidate_1=mpz(1)
+    candidate_2=mpz(100000)
+    candidate_3=mpz(100000000000000) # the number assigned to the candidate is 10^i+2
     blank_vote=mpz(1)
 
     #initialize the sum
@@ -44,6 +46,10 @@ def decipher():
 def main():
     #test
     initialize()
+    candidate_1=mpz(10)
+    candidate_2=mpz(100000)
+    candidate_3=mpz(100000000000000) # the number assigned to the candidate is 10^i+2
+    blank_vote=mpz(1)
     votes=[('A',candidate_3), ('B',candidate_2),('C',candidate_3),
        ('D',candidate_2),('E',candidate_3),('F',candidate_1),
        ('G',candidate_3), ('H',candidate_2),('J',candidate_3),
@@ -54,3 +60,7 @@ def main():
         update_new_vote()
     print("chiffre sum:  \n{0}".format(sum))
     decipher()
+
+
+if __name__ == "__main__":
+    main()
