74a79e1e4f5842ed8093b4e3fb0f0ef7d6cd34f0
KatonCat <95046461+KatonCat@users.noreply.github.com>
2023-05-02 17:10:35 +0200

    Generate token and verify token files

---
+++ generate_token.py
@@ -0,0 +1,46 @@
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization, hashes
+import os
+import verify_token as vf
+tokens = []
+def generate_token(login, private_key):
+    random_value = os.urandom(16)
+    message = login.encode() + random_value
+    signature = private_key.sign(
+        message,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    login_len = len(login)
+    token = bytes([login_len]) + login.encode() + random_value + signature
+    return token.hex()
+
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048
+)
+public_key = private_key.public_key()
+
+public_key_pem = public_key.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+)
+
+token1 = generate_token("login", private_key)
+token2 = generate_token("login", private_key)
+
+is_valid1 = vf.verify_token(token1, public_key_pem,"login")
+is_valid2 = vf.verify_token(token2, public_key_pem,"login")
+
+if(is_valid1 and is_valid2):
+    print("Valid")
+    tokens.append(token1)
+    tokens.append(token2)
+    print("List of tokens:")
+    for token in tokens:
+        print(token)
+else:
+    print("Not Valid")
---
+++ verify_token.py
@@ -0,0 +1,32 @@
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.backends import default_backend
+import os
+import token_list as tl
+
+
+
+def verify_token(token, public_key_pem, login):
+    public_key = serialization.load_pem_public_key(
+        public_key_pem,
+        backend=default_backend()
+    )
+    token_bytes = bytes.fromhex(token)
+    login_len = token_bytes[0]
+    login_bytes = token_bytes[1:login_len+1]
+    random_value = token_bytes[login_len+1:login_len+17]
+    signature = token_bytes[login_len+17:]
+    message = login_bytes + random_value
+    try:
+        public_key.verify(
+            signature,
+            message,
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+        return True
+    except:
+        return False
