a6feb3a867188ff196ab7b9760a1dea0b39e80af
Cayla-Ginestet Thomas <caylagin@insa-11272>
2023-05-18 17:00:26 +0200

    modification organisation

--- Serveur_Verif/resultats.py
+++ Serveur_Verif/resultats.py
@@ -12,11 +12,15 @@ from decipher import decipher
 from paillierDiviseur import division
 
 def giveResults() :
+    print("RESULTATS")
     votes = dbVotes.getAllVotes()
+    print("\nNombre de bulletins de vote : " + str(len(votes)) + "\n")
     key_pair = get_key_pair()
     public_key = key_pair.public_key
     
-    #pas donner la clé privée au compteur
+   
+    print("Voici la liste des bulletins de vote :")
+     #pas donner la clé privée au compteur
     sum = paillier_compteur.compteur (votes,public_key)
     results = decipher(sum,key_pair)
     print("\nresultsss " + str(results))
